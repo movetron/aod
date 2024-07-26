26c9150a04cee295709434e0a2231713c1af22e4
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2024-07-26 17:22:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//@prepros-append jq-start.js
+//@prepros-append script.js
+//@prepros-append jq-end.js
+
+
+$('.icon-menu').click(function(event){
+	$(this).toggleClass('active');
+	$('.menu__body').toggleClass('active');
+	$('.body').toggleClass('lock');
+});
+$(document).ready(function(){
+	$('.aod__content').slick({
+		arrows:true,
+		dots:true,
+		adaptiveHeight:true,
+		slidesToShow:1,
+		sldesToScroll:0,
+		speed:1000,
+		easing:'ease',
+		infinite:true,
+		initialSlide:0,
+		autoplay:true,
+		autoplaySpeed:1000,
+		pauseOnFocus:true,
+		pauseOnHover:true,
+		pauseOnDotHover:true,
+		draggable:false,
+		swipe:true,
+		touchMove:true,
+		waitForAnimate:false,
+		centerMode:false,
+		variableWidth:false ,
+	});
+});
+
